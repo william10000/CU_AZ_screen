61da908a730cae3334d82fe0be28d45697eb22f9
unknown <william10000@gmail.com>
2015-03-27 07:08:21 -0600

    Initial uploaded files

--- 150315_analysis_v01.R
+++ 150315_analysis_v01.R
@@ -3,6 +3,9 @@
 # cells segmented & data exported on 2015-03-15
 # script copied/created on 2015-03-15
 
+# 2015-03-15: moved functions to separate script file
+# 2015-03-15: removed points based on histograms
+
 # loads raw data files and makes plots - particle sizes to exclude were
 # determined manually by looking at several images from several groups
 
@@ -10,94 +13,19 @@
 # cells per well, number of cells per experimenta condition
 
 # TODO: ANOVA, LM, Box plots, histograms of different experimental groups
-#
+# plots to make - 0nm case only; diameter, area, cell # vs. log(dose response)
+
 
 library(ggplot2)
+library(lattice)
 
 rm(list = ls())
+graphics.off()
 
-# define minimum and maximum sizes (manually determined for now)
-cell.min <- 150
-cell.max <- 1000
-
-clean.raw <- function(x) { # where x is the data frame for the raw data
-  names(x)[6] <- "area"
-#   temp <- subset(x, select = c(Well.Name, Site.ID, Cell.ID, area))
-  temp <- x
-  temp$area <- as.numeric(temp$area)
-  temp$diameter <- sqrt(temp$area/pi)
-  temp <- temp[!((temp$area < cell.min) | (temp$area > cell.max)), ]
-  return(temp)
-}
-
-assign.treatment.all <- function(plate, well) { # (plate a) where x is the vector of $Well.Name
-  temp.a <- rep(0, length(plate)) # dose of drug a
-  temp.b <- rep(NA, length(plate)) # name of drug a
-  temp.c <- rep("none", length(plate)) # name of drug b
-  
-  # assign AZD conc. based on columns on the plate
-  temp.a[grepl("02", well)] <- 0
-  temp.a[grepl("03", well)] <- 10
-  temp.a[grepl("04", well)] <- 30
-  temp.a[grepl("05", well)] <- 100
-  temp.a[grepl("06", well)] <- 300
-  temp.a[grepl("07", well)] <- 1000
-  temp.a[(grepl("09", well) & grepl("2768", plate))] <- 0
-  temp.a[(grepl("10", well) & grepl("2768", plate))] <- 10
-  temp.a[(grepl("11", well) & grepl("2768", plate))] <- 30
-  temp.a[(grepl("09", well) & grepl("2767", plate))] <- 100
-  temp.a[(grepl("10", well) & grepl("2767", plate))] <- 300
-  temp.a[(grepl("11", well) & grepl("2767", plate))] <- 1000
-  
-  # assign AZD  
-  temp.b[grepl("2767|2768", plate)] <- "AZD" 
-  temp.b[(grepl("02|03|04|05|06|07|", well) & grepl("2769|2770", plate))] <- "AZD"
-  
-  # assign CSA
-  temp.b[(grepl("09", well) & grepl("2770", plate))] <- "CSA"
-  temp.b[(grepl("08", well) & grepl("2769", plate))] <- "CSA"
-  
-  # assign ET-1
-  temp.c[(grepl("09|10|11", well) & grepl("2768|2767", plate))] <- "ET-1"
-  
-  # assign FGF-23
-  temp.c[(grepl("02|03|04|05|06|07|08", well) & grepl("2769", plate))] <- "FGF-23"
-  
-  # assign PE
-  temp.c[(grepl("02|03|04|05|06|07", well) & grepl("2768", plate))] <- "PE"
-  
-  # assign IGF-1
-  temp.c[(grepl("02|03|04|05|06|07", well) & grepl("2767", plate))] <- "IGF-1"  
-  
-  return(as.data.frame(cbind(temp.b, temp.a, temp.c)))
-}
-
-sum.area <- function(x) { # where x is the data frame to be analyzed
-  temp <- aggregate(area ~ drug.a * dose.a * drug.b, data = x, mean)
-  temp$sd <- aggregate(area ~ drug.a * dose.a * drug.b, data = x, sd)$area
-  temp$n <- aggregate(area ~ drug.a * dose.a * drug.b, data = x, length)$area
-  temp$sem <- temp$sd / sqrt(temp$n)
-  return(temp)
-}
-
-sum.diameter <- function(x) { # where x is the data frame to be analyzed
-  temp <- aggregate(diameter ~ drug.a * dose.a * drug.b, data = x, mean)
-  temp$sd <- aggregate(diameter ~ drug.a * dose.a * drug.b, data = x, sd)$diameter
-  temp$n <- aggregate(diameter ~ drug.a * dose.a * drug.b, data = x, length)$diameter
-  temp$sem <- temp$sd / sqrt(temp$n)
-  return(temp)
-}
-
-sum.intensity <- function(x) { # where x is the data frame to be analyzed
-  temp <- aggregate(average.intensity ~ drug.a * dose.a * drug.b, data = x, mean)
-  temp$sd <- aggregate(average.intensity ~ drug.a * dose.a * drug.b, data = x, sd)$average.intensity
-  temp$n <- aggregate(average.intensity ~ drug.a * dose.a * drug.b, data = x, length)$average.intensity
-  temp$sem <- temp$sd / sqrt(temp$n)
-  return(temp)
-}
+source("150315_functions_v01.R")
 
 # load tab delimited files
-raw.all <- read.delim("150306_all_plates.txt") # all plates
+raw.all <- read.delim("150315_all_AZ_plates.txt") # all plates
 
 # clean up raw data, remove outliers, and calculate "diameter"
 temp.all <- clean.raw(raw.all)
@@ -111,65 +39,32 @@ temp.all$dose.a <- as.numeric(as.character(temp.all$dose.a))
 area <- sum.area(temp.all)
 diameter <- sum.diameter(temp.all)
 
+# create data frame for 0nM groups
+diameter.zero <- diameter[(diameter$dose.a == 0), ]
+area.zero <- area[(area$dose.a == 0), ]
+
+# plot histograms
+hist(as.numeric(raw.all$W1.Stained.Area..MultiWaveScoring.), 100)
+hist(as.numeric(raw.all$Cell..W2.Stained.Area..MultiWaveScoring.), 100)
+
+hist(temp.all$nucleus, 100)
+hist(temp.all$cytoplasm, 100)
+hist(temp.all$area, 100)
+
 # plots
 
-ggplot(data = temp.all, aes(x = dose.a, y = area)) +
-  geom_bar(stat = "identity", position = position_dodge()) + theme(legend.position = "right") +
-  xlab("AZD dose (nM) ") + ylab("Mean area (um^2)") + 
-  ggtitle("Area vs. AZD dose") + theme_bw()
+# ggplot(data = temp.all, aes(x = dose.a, y = area)) +
+#   geom_bar(stat = "identity", position = position_dodge()) + theme(legend.position = "right") +
+#   xlab("AZD dose (nM) ") + ylab("Mean area (um^2)") + 
+#   ggtitle("Area vs. AZD dose") + theme_bw()
 #   geom_errorbar(aes(ymax = area + AI_norm.sem, ymin = AI_norm - AI_norm.sem,), position = "dodge")
 
 # write processed raw data to file to check processing
 write.table(temp.all, "all_plates_processed.txt")
+write.csv(area, "150315_area.csv")
+write.csv(diameter, "150315_diameter.csv")
 
 # ------ old code ------
 # 
-# assign.treatment.b <- function(x) { # (plate b) where x is the vector of $Well.Name
-#   temp.a <- rep(NA, length(x)) # dose of drug a
-#   temp.b <- rep(NA, length(x)) # name of drug a
-#   temp.c <- rep(NA, length(x)) # name of drug b
-#   temp.a[grepl("02|09", x)] <- 0 # assign AZD conc. based on columns on the plate
-#   temp.a[grepl("03|10", x)] <- 10
-#   temp.a[grepl("04|11", x)] <- 30
-#   temp.a[grepl("05", x)] <- 100
-#   temp.a[grepl("06", x)] <- 300
-#   temp.a[grepl("07", x)] <- 1000
-#   temp.b <- rep("AZD", length(x)) # AZD used in all wells
-#   temp.c <- rep("PE", length(x)) # initialize with PE
-#   temp.c[grepl("09|10|11", x)] <- "ET1" # assign columns 9-11 to ET-1
-#   return(as.data.frame(cbind(temp.b, temp.a, temp.c)))
-# }
-# 
-# assign.treatment.c <- function(x) { # (plate c) where x is the vector of $Well.Name
-#   temp.a <- rep(NA, length(x)) # dose of drug a
-#   temp.b <- rep(NA, length(x)) # name of drug a
-#   temp.c <- rep(NA, length(x)) # name of drug b
-#   temp.a[grepl("02|08", x)] <- 0 # assign AZD conc. based on columns on the plate
-#   temp.a[grepl("03", x)] <- 10
-#   temp.a[grepl("04", x)] <- 30
-#   temp.a[grepl("05", x)] <- 100
-#   temp.a[grepl("06", x)] <- 300
-#   temp.a[grepl("07", x)] <- 1000
-#   temp.b <- rep("AZD", length(x)) # initialize with AZD
-#   temp.b[grepl("08", x)] <- "CSA"
-#   temp.c <- rep("FGF", length(x)) # initialize with IGF-1
-#   return(as.data.frame(cbind(temp.b, temp.a, temp.c)))
-# }
-# 
-# assign.treatment.d <- function(x) { # (plate d) where x is the vector of $Well.Name
-#   temp.a <- rep(NA, length(x)) # dose of drug a
-#   temp.b <- rep(NA, length(x)) # name of drug a
-#   temp.c <- rep(NA, length(x)) # name of drug b
-#   temp.a[grepl("02|09", x)] <- 0 # assign AZD conc. based on columns on the plate
-#   temp.a[grepl("03", x)] <- 10
-#   temp.a[grepl("04", x)] <- 30
-#   temp.a[grepl("05", x)] <- 100
-#   temp.a[grepl("06", x)] <- 300
-#   temp.a[grepl("07", x)] <- 1000
-#   temp.b <- rep("AZD", length(x)) # initialize with AZD
-#   temp.b[grepl("09", x)] <- "CSA"
-#   temp.c <- rep("None", length(x)) # initialize with NA
-#   return(as.data.frame(cbind(temp.b, temp.a, temp.c)))
-# }
 
 
